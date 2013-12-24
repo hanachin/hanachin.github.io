03b7970b721d188236e2298c0f28de144e744f89
Seiei Higa <hanachin@gmail.com>
2013-12-24 22:28:30 +0900

    トップディレクトリにサイトを生成せずbuild以下に生成するよう設定を変更し、生成されたindex.htmlを削除した

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -2,7 +2,6 @@ require File.expand_path '../hanachin', __dir__
 require 'sitespec'
 
 Sitespec.configuration.application = Hanachin.new
-Sitespec.configuration.build_path  = '.'
 
 describe 'hanachin.github.io' do
   include Sitespec
